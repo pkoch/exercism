1351c948898dc41701b56014f962e99d8bf179d0
Paulo Köch <paulo.koch@gmail.com>
2019-08-25 17:24:01 +0200

    rust: proverb: railroad

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,16 +1,13 @@
+use std::iter::once;
+
 pub fn build_proverb(list: &[&str]) -> String {
     if list.is_empty() {
         return String::new();
     };
 
-    let mut res = list
-        .windows(2)
-        .map(|pair| match pair {
-            [a, b] => format!("For want of a {} the {} was lost.", a, b),
-            other => panic!("Wierd window! `{:?}`", other),
-        })
-        .collect::<Vec<String>>();
-
-    res.push(format!("And all for the want of a {}.", list[0]));
-    res.join("\n")
+    list.windows(2)
+        .map(|pair| format!("For want of a {} the {} was lost.", pair[0], pair[1]))
+        .chain(once(format!("And all for the want of a {}.", list[0])))
+        .collect::<Vec<String>>()
+        .join("\n")
 }
