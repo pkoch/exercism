eb94d2c2bd7156dc0f43b7bc7134aa8c36a493e8
Paulo Köch <paulo.koch@gmail.com>
2019-08-25 17:25:37 +0200

    rust: proverb: use NonEmptyOr

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,13 +1,24 @@
 use std::iter::once;
 
-pub fn build_proverb(list: &[&str]) -> String {
-    if list.is_empty() {
-        return String::new();
-    };
+pub trait NonEmptyOr {
+    fn non_empty_or(self: Self, alternative: Self) -> Self;
+}
 
+impl NonEmptyOr for String {
+    fn non_empty_or(self: Self, alternative: Self) -> Self{
+        if !self.is_empty() {
+            return self;
+        };
+
+        alternative
+    }
+}
+
+pub fn build_proverb(list: &[&str]) -> String {
     list.windows(2)
         .map(|pair| format!("For want of a {} the {} was lost.", pair[0], pair[1]))
         .chain(once(format!("And all for the want of a {}.", list[0])))
         .collect::<Vec<String>>()
         .join("\n")
+        .non_empty_or(String::new())
 }
