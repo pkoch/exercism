4e43f10cfd1051969141c01863dce6b932b9ade9
Paulo Köch <paulo.koch@gmail.com>
2020-09-20 15:33:56 +0100

    Style

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -1,16 +1,16 @@
 pub fn verse(n: i32) -> String {
     match n {
         0 => [
-            "No more bottles of beer on the wall, no more bottles of beer.\n".to_string(),
-            "Go to the store and buy some more, 99 bottles of beer on the wall.\n".to_string(),
+            "No more bottles of beer on the wall, no more bottles of beer.\n".into(),
+            "Go to the store and buy some more, 99 bottles of beer on the wall.\n".into(),
         ],
         1 => [
-            "1 bottle of beer on the wall, 1 bottle of beer.\n".to_string(),
-            "Take it down and pass it around, no more bottles of beer on the wall.\n".to_string(),
+            "1 bottle of beer on the wall, 1 bottle of beer.\n".into(),
+            "Take it down and pass it around, no more bottles of beer on the wall.\n".into(),
         ],
         2 => [
-            "2 bottles of beer on the wall, 2 bottles of beer.\n".to_string(),
-            "Take one down and pass it around, 1 bottle of beer on the wall.\n".to_string(),
+            "2 bottles of beer on the wall, 2 bottles of beer.\n".into(),
+            "Take one down and pass it around, 1 bottle of beer on the wall.\n".into(),
         ],
         n => [
             format!("{0} bottles of beer on the wall, {0} bottles of beer.\n", n),
@@ -19,7 +19,8 @@ pub fn verse(n: i32) -> String {
                 n - 1
             ),
         ],
-    }.join("")
+    }
+    .join("")
 }
 
 pub fn sing(start: i32, end: i32) -> String {
