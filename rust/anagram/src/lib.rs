7d37e2ebbf8a35e81431ddec470fbedc8ef8a846
Paulo Köch <paulo.koch@gmail.com>
2021-10-11 00:53:20 +0100

    Finish anagram

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,9 +1,23 @@
 use std::collections::HashSet;
+use unicode_segmentation::UnicodeSegmentation;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    unimplemented!(
-        "For the '{}' word find anagrams among the following words: {:?}",
-        word,
-        possible_anagrams
-    );
+fn sorted_chars(s: &str) -> String {
+    let mut v = s.graphemes(true)
+        .map(|a| a.to_lowercase())
+        .collect::<Vec<String>>();
+    v.sort();
+    v.join("")
+}
+
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&str]) -> HashSet<&'a str> {
+    let lower_word = word.to_lowercase();
+
+    possible_anagrams
+        .iter()
+        .filter(|s| {
+            let sl = s.to_lowercase();
+            sl != lower_word && sorted_chars(&sl) == sorted_chars(word)
+        })
+        .map(|a| *a)
+        .collect::<HashSet<&str>>()
 }
