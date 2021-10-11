be2148c3341bebcc6fabe97c442191952bd2887d
Paulo Köch <paulo.koch@gmail.com>
2021-10-11 01:11:14 +0100

    Linting

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -2,21 +2,21 @@ use std::collections::HashSet;
 use unicode_segmentation::UnicodeSegmentation;
 
 fn sorted_chars(s: &str) -> String {
-    let mut v = s.graphemes(true)
-        .collect::<Vec<_>>();
-    v.sort();
+    let mut v = s.graphemes(true).collect::<Vec<_>>();
+    v.sort_unstable();
     v.join("")
 }
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&str]) -> HashSet<&'a str> {
     let lower_word = word.to_lowercase();
+    let sorted_lower_word = sorted_chars(&lower_word);
 
     possible_anagrams
         .iter()
-        .filter(|s| {
-            let sl = s.to_lowercase();
-            sl != lower_word && sorted_chars(&sl) == sorted_chars(&lower_word)
+        .filter(|o| {
+            let ol = o.to_lowercase();
+            ol != lower_word && sorted_chars(&ol) == sorted_lower_word
         })
-        .map(|a| *a)
-        .collect::<HashSet<&str>>()
+        .cloned()
+        .collect()
 }
