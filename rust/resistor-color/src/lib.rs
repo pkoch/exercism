218784ee376bcc862d5d8f5e89437b0175e504ce
Paulo Köch <paulo.koch@gmail.com>
2022-07-08 01:45:20 +0100

    Done rust/resistor-color

--- rust/resistor-color/src/lib.rs
+++ rust/resistor-color/src/lib.rs
@@ -1,28 +1,44 @@
-#[derive(Debug, PartialEq)]
+use strum::FromRepr;
+use strum::Display;
+
+#[derive(Display, FromRepr, Debug, PartialEq)]
+#[repr(usize)]
 pub enum ResistorColor {
     Black,
-    Blue,
     Brown,
-    Green,
-    Grey,
-    Orange,
     Red,
-    Violet,
-    White,
+    Orange,
     Yellow,
+    Green,
+    Blue,
+    Violet,
+    Grey,
+    White,
 }
 
-pub fn color_to_value(_color: ResistorColor) -> usize {
-    unimplemented!("convert a color into a numerical representation")
+pub fn color_to_value(color: ResistorColor) -> usize {
+    color as usize
 }
 
 pub fn value_to_color_string(value: usize) -> String {
-    unimplemented!(
-        "convert the value {} into a string representation of color",
-        value
-    )
+    match ResistorColor::from_repr(value) {
+        Some(e) => e.to_string(),
+        None => "value out of range".to_string(),
+    }
+
 }
 
 pub fn colors() -> Vec<ResistorColor> {
-    unimplemented!("return a list of all the colors ordered by resistance")
+    vec![
+        ResistorColor::Black,
+        ResistorColor::Brown,
+        ResistorColor::Red,
+        ResistorColor::Orange,
+        ResistorColor::Yellow,
+        ResistorColor::Green,
+        ResistorColor::Blue,
+        ResistorColor::Violet,
+        ResistorColor::Grey,
+        ResistorColor::White,
+    ]
 }
