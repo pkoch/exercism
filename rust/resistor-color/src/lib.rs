7e3a5525c3352adf15e7d921ba15d04de1980d79
Paulo Köch <paulo.koch@gmail.com>
2022-07-08 01:55:26 +0100

    Use suggested crates

--- rust/resistor-color/src/lib.rs
+++ rust/resistor-color/src/lib.rs
@@ -1,19 +1,19 @@
-use strum::FromRepr;
-use strum::Display;
+use enum_iterator::{all, Sequence};
+use int_enum::IntEnum;
 
-#[derive(Display, FromRepr, Debug, PartialEq)]
+#[derive(Debug, PartialEq, Sequence, Copy, Clone, IntEnum)]
 #[repr(usize)]
 pub enum ResistorColor {
-    Black,
-    Brown,
-    Red,
-    Orange,
-    Yellow,
-    Green,
-    Blue,
-    Violet,
-    Grey,
-    White,
+    Black = 0,
+    Brown = 1,
+    Red = 2,
+    Orange = 3,
+    Yellow = 4,
+    Green = 5,
+    Blue = 6,
+    Violet = 7,
+    Grey = 8,
+    White = 9,
 }
 
 pub fn color_to_value(color: ResistorColor) -> usize {
@@ -21,24 +21,22 @@ pub fn color_to_value(color: ResistorColor) -> usize {
 }
 
 pub fn value_to_color_string(value: usize) -> String {
-    match ResistorColor::from_repr(value) {
-        Some(e) => e.to_string(),
-        None => "value out of range".to_string(),
-    }
+    match value {
+        0 => "Black",
+        1 => "Brown",
+        2 => "Red",
+        3 => "Orange",
+        4 => "Yellow",
+        5 => "Green",
+        6 => "Blue",
+        7 => "Violet",
+        8 => "Grey",
+        9 => "White",
+        _ => "value out of range",
+    }.to_string()
 
 }
 
 pub fn colors() -> Vec<ResistorColor> {
-    vec![
-        ResistorColor::Black,
-        ResistorColor::Brown,
-        ResistorColor::Red,
-        ResistorColor::Orange,
-        ResistorColor::Yellow,
-        ResistorColor::Green,
-        ResistorColor::Blue,
-        ResistorColor::Violet,
-        ResistorColor::Grey,
-        ResistorColor::White,
-    ]
+    all::<ResistorColor>().collect()
 }
