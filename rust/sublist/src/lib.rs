c6e6f74f28f785acb1ced0735040d9565e71725a
Paulo Köch <paulo.koch@gmail.com>
2022-07-09 19:34:20 +0100

    Solve rust/sublist

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -1,3 +1,5 @@
+use std::cmp::min;
+
 #[derive(Debug, PartialEq)]
 pub enum Comparison {
     Equal,
@@ -6,6 +8,51 @@ pub enum Comparison {
     Unequal,
 }
 
-pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    unimplemented!("Determine if the first list is equal to, sublist of, superlist of or unequal to the second list.");
+pub fn first_positions<T: PartialOrd>(l: &[T], target: &T) -> Vec<usize> {
+    l.iter()
+        .enumerate()
+        .filter(|(_i, v)| *v == target)
+        .map(|(i, _v)| i)
+        .collect()
+}
+
+pub fn shared_prefix_len<T: PartialOrd>(la: &[T], lb: &[T]) -> usize {
+    let mut it = la
+        .iter()
+        .zip(lb)
+        .enumerate()
+        .skip_while(|(_i, (a, b))| *a == *b);
+
+    match it.next() {
+        Some((i, _)) => i - 1,
+        None => min(la.len(), lb.len()),
+    }
+}
+
+pub fn is_substreak_of<T: PartialOrd>(a: &[T], b: &[T]) -> bool {
+    if a.len() == 0 {
+        return true;
+    }
+    if b.len() == 0 {
+        return false;
+    }
+    let &first_a = &a.get(0).unwrap();
+    let len_a = a.len();
+
+    first_positions(b, first_a)
+        .iter()
+        .map(|n| &b[*n..])
+        .any(|bi| shared_prefix_len(a, bi) == len_a)
+}
+
+pub fn sublist<T: PartialOrd>(first_list: &[T], second_list: &[T]) -> Comparison {
+    match (
+        is_substreak_of(first_list, second_list),
+        is_substreak_of(second_list, first_list),
+    ) {
+        (true, true) => Comparison::Equal,
+        (true, false) => Comparison::Sublist,
+        (false, true) => Comparison::Superlist,
+        (false, false) => Comparison::Unequal,
+    }
 }
