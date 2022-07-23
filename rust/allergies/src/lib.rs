4e612a783788e8d0236441cdd28cc9a82eaa89e3
Paulo Köch <paulo.koch@gmail.com>
2022-07-24 00:13:21 +0100

    Done

--- rust/allergies/src/lib.rs
+++ rust/allergies/src/lib.rs
@@ -1,33 +1,49 @@
-pub struct Allergies;
+use std::collections::HashSet;
 
-#[derive(Debug, PartialEq, Eq)]
-pub enum Allergen {
-    Eggs,
-    Peanuts,
-    Shellfish,
-    Strawberries,
-    Tomatoes,
-    Chocolate,
-    Pollen,
-    Cats,
+pub struct Allergies{
+    allergens: HashSet<Allergen>
 }
+
+#[derive(Debug, PartialEq, Eq, Hash, Copy, Clone)]
+#[repr(u32)]
+pub enum Allergen {
+    Eggs = 0b1,
+    Peanuts = 0b10,
+    Shellfish = 0b100,
+    Strawberries = 0b1000,
+    Tomatoes = 0b10_000,
+    Chocolate = 0b10_0000,
+    Pollen = 0b100_0000,
+    Cats = 0b1000_0000,
+}
+
+static ALL_ALLERGIES: [Allergen; 8] = [
+    Allergen::Eggs,
+    Allergen::Peanuts,
+    Allergen::Shellfish,
+    Allergen::Strawberries,
+    Allergen::Tomatoes,
+    Allergen::Chocolate,
+    Allergen::Pollen,
+    Allergen::Cats,
+];
 
 impl Allergies {
     pub fn new(score: u32) -> Self {
-        unimplemented!(
-            "Given the '{}' score, construct a new Allergies struct.",
-            score
-        );
+        Allergies{
+            allergens: ALL_ALLERGIES
+                .iter()
+                .filter(|a| (**a as u32) & score != 0)
+                .map(|a| *a)
+                .collect()
+        }
     }
 
     pub fn is_allergic_to(&self, allergen: &Allergen) -> bool {
-        unimplemented!(
-            "Determine if the patient is allergic to the '{:?}' allergen.",
-            allergen
-        );
+        self.allergens.contains(allergen)
     }
 
     pub fn allergies(&self) -> Vec<Allergen> {
-        unimplemented!("Return the list of allergens contained within the score with which the Allergies struct was made.");
+        self.allergens.iter().map(|a| *a).collect()
     }
 }
