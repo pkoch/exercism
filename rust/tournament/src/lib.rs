3952dc7fef4d082c3b968fcb964d9d60e7ddc811
Paulo Köch <paulo.koch@gmail.com>
2022-07-24 02:29:55 +0100

    Less copying

--- rust/tournament/src/lib.rs
+++ rust/tournament/src/lib.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, str::FromStr};
+use std::{collections::HashMap, fmt::Display, str::FromStr};
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 enum Outcome {
@@ -42,37 +42,42 @@ impl FromStr for Outcome {
 }
 
 #[derive(Debug)]
-struct Match {
-    home: String,
-    away: String,
+struct Match<'a> {
+    home: &'a str,
+    away: &'a str,
     outcome: Outcome,
 }
 
-impl Match {
-    fn outcome_for(&self, team: &str) -> Outcome {
-        if self.home == team {
-            return self.outcome;
-        }
-        if self.away == team {
-            return self.outcome.flip();
-        }
-        panic!("{:?} is not a team involved in {:?}", team, self)
-    }
-}
-
-impl FromStr for Match {
-    type Err = String;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let atoms: Vec<&str> = s.splitn(3, ';').collect();
+impl<'a> Match<'a> {
+    fn from_str<'b: 'a>(s: &'b str) -> Result<Self, String> {
+        let atoms: Vec<&'b str> = s.splitn(3, ';').collect();
         if atoms.len() != 3 {
             return Err(format!("{:?} must have 3 fields separated by ';'.", s));
         };
-        Ok(Match {
-            home: atoms[0].to_string(),
-            away: atoms[1].to_string(),
+        Ok(Match::<'a> {
+            home: atoms[0],
+            away: atoms[1],
             outcome: atoms[2].parse()?,
         })
+    }
+}
+
+struct LeaderboardEntry<'a> {
+    team: &'a str,
+    mp: i32,
+    w: i32,
+    d: i32,
+    l: i32,
+    p: i32,
+}
+
+impl<'a> Display for LeaderboardEntry<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{:<30} | {:>2} | {:>2} | {:>2} | {:>2} | {:>2}",
+            self.team, self.mp, self.w, self.l, self.d, self.p,
+        )
     }
 }
 
@@ -84,47 +89,31 @@ pub fn tally(match_results: &str) -> String {
         .filter(|s| *s != "")
         .map(|l| Match::from_str(l).unwrap())
         .collect();
-    let team_involvements: HashMap<&str, Vec<&Match>> = matches
+
+    let team_outcomes: HashMap<&str, Vec<Outcome>> = matches
         .iter()
-        .flat_map(|m| [(m.home.as_str(), m), (m.away.as_str(), m)])
-        .fold(HashMap::<&str, Vec<&Match>>::new(), |mut ti, (t, m)| {
+        .flat_map(|m| [(m.home, m.outcome), (m.away, m.outcome.flip())])
+        .fold(HashMap::<&str, Vec<Outcome>>::new(), |mut ti, (t, m)| {
             ti.entry(t).or_default().push(m);
             ti
         });
-    let mut leaderboard_entries = team_involvements
-    .iter()
-    .map(|(team, involvements)| {
-        let outcomes: Vec<Outcome> = involvements
-            .iter()
-            .map(|i| i.outcome_for(team))
-            .collect();
-        (
-            *team,
-            outcomes.len() as i32,
-            outcomes.iter().filter(|o| **o == Win).count() as i32,
-            outcomes.iter().filter(|o| **o == Loss).count() as i32,
-            outcomes.iter().filter(|o| **o == Draw).count() as i32,
-            outcomes.iter().map::<i32, _>(|o| o.into()).sum::<i32>(),
-        )
-    })
-    .collect::<Vec<(&str, i32, i32, i32, i32, i32)>>();
-    leaderboard_entries.sort_by_key(|t| (t.5 * -1, t.0));
 
-    let mut res = vec![HEADER.to_string()];
-    res.extend(leaderboard_entries
+    let mut leaderboard_entries = team_outcomes
         .iter()
-        .map(|(team, mp, w, d, l, p)| {
-            format!(
-                "{:<30} | {:>2} | {:>2} | {:>2} | {:>2} | {:>2}",
-                team,
-                mp,
-                w,
-                l,
-                d,
-                p,
-            )
+        .map(|(team, outcomes)| LeaderboardEntry {
+            team: *team,
+            mp: outcomes.len() as i32,
+            w: outcomes.iter().filter(|o| **o == Win).count() as i32,
+            d: outcomes.iter().filter(|o| **o == Loss).count() as i32,
+            l: outcomes.iter().filter(|o| **o == Draw).count() as i32,
+            p: outcomes.iter().map::<i32, _>(|o| o.into()).sum(),
         })
+        .collect::<Vec<LeaderboardEntry>>();
+    leaderboard_entries.sort_by_key(|le| (le.p * -1, le.team));
+
+    vec![HEADER.to_string()]
+        .into_iter()
+        .chain(leaderboard_entries.iter().map(LeaderboardEntry::to_string))
         .collect::<Vec<String>>()
-    );
-    res.join("\n")
+        .join("\n")
 }
