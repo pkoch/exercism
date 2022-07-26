b71c89ac4349093841b54b6f666bbd5b5d27e6dc
Paulo Köch <paulo.koch@gmail.com>
2022-07-26 20:45:05 +0100

    Remove rt and lb

--- rust/rectangles/src/lib.rs
+++ rust/rectangles/src/lib.rs
@@ -9,21 +9,29 @@ pub struct Coordinate {
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Copy)]
 pub struct Rectangle {
     pub lt: Coordinate,
-    pub rt: Coordinate,
-    pub lb: Coordinate,
     pub rb: Coordinate,
 }
 
 impl Rectangle {
+    pub fn l(&self) -> usize {
+        self.lt.x
+    }
+    pub fn r(&self) -> usize {
+        self.rb.x
+    }
+    pub fn t(&self) -> usize {
+        self.lt.y
+    }
+    pub fn b(&self) -> usize {
+        self.rb.y
+    }
     /// ```
     /// # use rectangles::*;
     /// assert!(
     ///     Rectangle{
     ///         lt: Coordinate{x: 0, y: 0},
-    ///         rt: Coordinate{x: 1, y: 0},
-    ///         lb: Coordinate{x: 0, y: 1},
     ///         rb: Coordinate{x: 1, y: 1},
-    ///     }.is_connected(&[
+    ///     }.exists_in_grid(&[
     ///         "++",
     ///         "++",
     ///     ]),
@@ -31,10 +39,8 @@ impl Rectangle {
     /// assert!(
     ///     Rectangle{
     ///         lt: Coordinate{x: 0, y: 0},
-    ///         rt: Coordinate{x: 3, y: 0},
-    ///         lb: Coordinate{x: 0, y: 1},
     ///         rb: Coordinate{x: 3, y: 1},
-    ///     }.is_connected(&[
+    ///     }.exists_in_grid(&[
     ///         "+--+",
     ///         "+--+",
     ///     ]),
@@ -79,31 +85,32 @@ impl Rectangle {
     ///   assert_eq!(
     ///       Rectangle{
     ///           lt: Coordinate{x: 0, y: 0},
-    ///           rt: Coordinate{x: 2, y: 0},
-    ///           lb: Coordinate{x: 0, y: 2},
     ///           rb: Coordinate{x: 2, y: 2},
-    ///       }.is_connected(&grid[..]),
+    ///       }.exists_in_grid(&grid[..]),
     ///       *result,
     ///       "{:?} failed", n,
     ///   );
     /// }
     /// ```
-    pub fn is_connected(&self, lines: &[&str]) -> bool {
-        [
-            ((self.lt.x, self.rt.x), self.lt.y),
-            ((self.lb.x, self.rb.x), self.lb.y),
-        ]
-        .iter()
-        .all(|((x_start, x_end), y)| {
-            (*x_start..=*x_end).all(|x| ['-', '+'].contains(&char_at(lines, x, *y).unwrap_or(' ')))
-        }) && [
-            (self.lt.x, (self.lt.y, self.lb.y)),
-            (self.rt.x, (self.rt.y, self.rb.y)),
-        ]
-        .iter()
-        .all(|(x, (y_start, y_end))| {
-            (*y_start..=*y_end).all(|y| ['|', '+'].contains(&char_at(lines, *x, y).unwrap_or(' ')))
-        })
+    pub fn exists_in_grid(&self, lines: &[&str]) -> bool {
+        Some('+') == char_at(lines, self.r(), self.t())
+        && Some('+') == char_at(lines, self.l(), self.b())
+        &&[
+                ((self.l(), self.r()), self.t()),
+                ((self.l(), self.r()), self.b()),
+            ]
+            .iter()
+            .all(|((x_start, x_end), y)| {
+                (*x_start..=*x_end).all(|x| ['-', '+'].contains(&char_at(lines, x, *y).unwrap_or(' ')))
+            })
+        && [
+                (self.l(), (self.t(), self.b())),
+                (self.r(), (self.t(), self.b())),
+            ]
+            .iter()
+            .all(|(x, (y_start, y_end))| {
+                (*y_start..=*y_end).all(|y| ['|', '+'].contains(&char_at(lines, *x, y).unwrap_or(' ')))
+            })
     }
 }
 
@@ -137,15 +144,8 @@ pub fn count(lines: &[&str]) -> u32 {
                 continue;
             }
 
-            let rt = &Coordinate { x: rb.x, y: lt.y };
-            let lb = &Coordinate { x: lt.x, y: rb.y };
-
-            if Some('+') != char_at(lines, rt.x, rt.y) || Some('+') != char_at(lines, lb.x, lb.y) {
-                continue;
-            }
-
-            let candidate = Rectangle{lt: *lt, rt: *rt, lb: *lb, rb: *rb};
-            if candidate.is_connected(lines) {
+            let candidate = Rectangle{lt: *lt, rb: *rb};
+            if candidate.exists_in_grid(lines) {
                 println!("{:?}", candidate);
                 detected_rectangles.insert(candidate);
             }
