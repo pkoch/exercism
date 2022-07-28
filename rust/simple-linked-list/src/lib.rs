41ecc4e69fdfc89a6b02a2dd24941638209c4161
Paulo Köch <paulo.koch@gmail.com>
2022-07-28 11:43:50 +0100

    Having fun

--- rust/simple-linked-list/src/lib.rs
+++ rust/simple-linked-list/src/lib.rs
@@ -58,6 +58,18 @@ impl<T> SimpleLinkedList<T> {
         }
         new
     }
+
+    /// ```
+    /// # use simple_linked_list::SimpleLinkedList;
+    /// let mut list: SimpleLinkedList<u32> = SimpleLinkedList::new();
+    /// list.push(1);
+    /// list.push(2);
+    /// list.push(3);
+    /// assert_eq!(list.iter_rev().collect::<Vec<_>>(), vec![&3,&2,&1]);
+    /// ```
+    pub fn iter_rev(&'_ self) -> RevIter<'_, T> {
+        RevIter { node: &self.tail }
+    }
 }
 
 impl<T> Default for SimpleLinkedList<T> {
@@ -82,26 +94,45 @@ impl<T> IntoIterator for SimpleLinkedList<T> {
 
     fn into_iter(self) -> Self::IntoIter {
         let v = self.rev();
-        IntoIter{node: v.tail}
+        IntoIter { node: v.tail }
     }
 }
 
 pub struct IntoIter<T> {
-    node: Option<Box<Node<T>>>
+    node: Option<Box<Node<T>>>,
 }
 
 impl<T> Iterator for IntoIter<T> {
     type Item = T;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let old_node = mem::take(&mut self.node);
-        if let None = old_node {
-            return None;
+        match mem::take(&mut self.node) {
+            None => None,
+            Some(old_box) => {
+                let res = Some(old_box.element);
+                self.node = old_box.prev;
+                res
+            }
         }
-        let old_box = old_node.unwrap();
-        let res = Some(old_box.element);
-        self.node = old_box.prev;
-        res
+    }
+}
+
+pub struct RevIter<'a, T> {
+    node: &'a Option<Box<Node<T>>>,
+}
+
+impl<'a, T> Iterator for RevIter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.node {
+            None => None,
+            Some(old_box) => {
+                let res = Some(&old_box.element);
+                self.node = &old_box.prev;
+                res
+            }
+        }
     }
 }
 
