5e261f98538e3abc84a24b092b478ec1817a1ba7
Paulo Köch <paulo.koch@gmail.com>
2022-11-03 23:16:24 +0000

    done

--- rust/low-power-embedded-game/src/lib.rs
+++ rust/low-power-embedded-game/src/lib.rs
@@ -1,21 +1,19 @@
-// This stub file contains items that aren't used yet; feel free to remove this module attribute
-// to enable stricter warnings.
-#![allow(unused)]
+use std::ops::Rem;
 
 pub fn divmod(dividend: i16, divisor: i16) -> (i16, i16) {
-    unimplemented!("implement `fn divmod`");
+    (dividend / divisor, dividend.rem(divisor))
 }
 
 pub fn evens<T>(iter: impl Iterator<Item = T>) -> impl Iterator<Item = T> {
-    unimplemented!("implement `fn evens`");
-    // TODO: remove this; it's only necessary to allow this function to compile
-    // before the student has done any work.
-    std::iter::empty()
+    iter.enumerate()
+        .filter(|(i, _v)| i.rem(2) == 0)
+        .map(|(_i, v)| v)
 }
 
 pub struct Position(pub i16, pub i16);
 impl Position {
     pub fn manhattan(&self) -> i16 {
-        unimplemented!("implement `fn manhattan`")
+        let Position(x, y) = self;
+        x.abs() + y.abs()
     }
 }
